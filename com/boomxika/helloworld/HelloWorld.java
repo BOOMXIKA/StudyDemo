75df477809d9554b5521f9c4caa8265f80c7df5a
BOOMXIKA <1148374607@qq.com>
2022-04-25 21:10:11 +0800

    init(快乐小天地): 首次提交

---
+++ com/boomxika/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.boomxika.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("开启我的快乐小天地");
+    }
+}
